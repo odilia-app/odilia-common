683181c45392768f6f812b8d4b438b93aa16a503
Michael Connor Buchan <mikey@blindcomputing.org>
2021-11-20 20:45:00 +0000

    Fix typo

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,28 @@
 pub enum ElementType {
-  Heading,
-  HeadingLevel1,
-  HeadingLevel2,
-  HeadingLevel3,
-  HeadingLevel4,
-  HeadingLevel5,
-  HeadingLevel6,
-  Button,
-  Text,
-  Table,
-  TableCell,
-  List,
-  ListItem,
-  Tab, // ?? is this what it is when you're looking at tabs in a dialog?
+    Heading,
+    HeadingLevel1,
+    HeadingLevel2,
+    HeadingLevel3,
+    HeadingLevel4,
+    HeadingLevel5,
+    HeadingLevel6,
+    Button,
+    Text,
+    Table,
+    TableCell,
+    List,
+    ListItem,
+    Tab, // ?? is this what it is when you're looking at tabs in a dialog?
 }
 
 pub enum ScreenReaderMode {
-  BorwseMode,
-  FocusMode,
-  ObjectNavigationMode,
+    BrowseMode,
+    FocusMode,
+    ObjectNavigationMode,
 }
 
 pub enum ScreenReaderEventType {
-  ChangeMode(ScreenReaderMode),
-  Next(ElementType),
-  Previous(ElementType),
+    ChangeMode(ScreenReaderMode),
+    Next(ElementType),
+    Previous(ElementType),
 }
-

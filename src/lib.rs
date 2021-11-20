0e778360d51e25f9bbbe388c03a9a3cce8d1bfdf
Tait Hoyem <tait@tait.tech>
2021-11-20 09:13:46 -0700

    add sources

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+enum ElementType {
+  Heading,
+  HeadingLevel1,
+  HeadingLevel2,
+  HeadingLevel3,
+  HeadingLevel4,
+  HeadingLevel5,
+  HeadingLevel6,
+  Button,
+  Text,
+  Table,
+  TableCell,
+  List,
+  ListItem,
+  Tab, // ?? is this what it is when you're looking at tabs in a dialog?
+}
+
+enum ScreenReaderMode {
+  BorwseMode,
+  FocusMode,
+  ObjectNavigationMode,
+}
+
+enum ScreenReaderEventType {
+  ChangeMode(ScreenReaderMode),
+  Next(ElementType),
+  Previous(ElementType),
+}
+
